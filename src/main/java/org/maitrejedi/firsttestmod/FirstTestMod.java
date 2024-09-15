0339ae07c06e813c6c90cc479f06898c06993a4e
Maitrejedi <evgegalas@gmail.com>
2024-09-15 16:46:13 +0400

    asd

--- src/main/java/org/maitrejedi/firsttestmod/FirstTestMod.java
+++ src/main/java/org/maitrejedi/firsttestmod/FirstTestMod.java
@@ -1,20 +1,39 @@
 package org.maitrejedi.firsttestmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.MovementInputUpdateEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import org.maitrejedi.firsttestmod.content.items.GlassBlockPlacerItem;
+import org.maitrejedi.firsttestmod.init.ItemsMI;
 import org.slf4j.Logger;
 import org.zeith.hammerlib.api.items.CreativeTab;
 import org.zeith.hammerlib.core.adapter.LanguageAdapter;
 import org.zeith.hammerlib.core.init.ItemsHL;
 import org.zeith.hammerlib.proxy.HLConstants;
 
+import java.util.List;
+
 @Mod(FirstTestMod.MOD_ID)
 public class FirstTestMod
 {
 	public static final String MOD_ID = "firsttestmod";
 	public static final Logger LOGGER = LogUtils.getLogger();
-	
+	private static final List<Block> allowedBlocks = List.of(Blocks.LAVA, Blocks.WATER, Blocks.AIR);
+
+	public FirstTestMod(IEventBus modBus) {
+		NeoForge.EVENT_BUS.addListener(FirstTestMod::placeGlassUnderPlayer);
+		LanguageAdapter.registerMod(MOD_ID);
+	}
+
 	/*@CreativeTab.RegisterTab
 	public static final CreativeTab MOD_TAB = new CreativeTab(id("root"),
 			builder -> builder
@@ -22,13 +41,25 @@ public class FirstTestMod
 					.withTabsBefore(HLConstants.HL_TAB.id())
 	);*/
 	
-	public FirstTestMod()
-	{
-		LanguageAdapter.registerMod(MOD_ID);
-	}
-	
 	/*public static ResourceLocation id(String path)
 	{
 		return new ResourceLocation(MOD_ID, path);
 	}*/
+
+	private static void placeGlassUnderPlayer(MovementInputUpdateEvent event) {
+		if(event.getEntity() instanceof Player) {
+			Player player = event.getEntity();
+			Inventory inventory = player.getInventory();
+			var item = ItemsMI.GLASS_BLOCK_PLACER;
+			if(inventory.contains(new ItemStack(item))) {
+				GlassBlockPlacerItem modItem = (GlassBlockPlacerItem) inventory.getItem(inventory.findSlotMatchingItem(new ItemStack(item))).getItem();
+				if(modItem.isActivated()) {
+					BlockPos bPos = player.blockPosition().below();
+					if(allowedBlocks.contains(player.level().getBlockState(bPos).getBlock())) {
+						player.level().setBlockAndUpdate(bPos, Blocks.GLASS.defaultBlockState());
+					}
+				}
+			}
+		}
+	}
 }
--- src/main/java/org/maitrejedi/firsttestmod/content/items/GlassBlockPlacerItem.java
+++ src/main/java/org/maitrejedi/firsttestmod/content/items/GlassBlockPlacerItem.java
@@ -1,16 +1,15 @@
 package org.maitrejedi.firsttestmod.content.items;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LiquidBlock;
 
 public class GlassBlockPlacerItem extends Item {
+
+    private boolean isActivated = false;
 
     public GlassBlockPlacerItem(Properties pProperties) {
         super(pProperties);
@@ -18,16 +17,33 @@ public class GlassBlockPlacerItem extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-        BlockPos bPos = pPlayer.getBlockPosBelowThatAffectsMyMovement();
         if(!pLevel.isClientSide) {
-            if(
-                    pLevel.getBlockState(bPos).isAir() ||
-                    pLevel.getBlockState(bPos).getBlock().equals(Blocks.WATER) ||
-                    pLevel.getBlockState(bPos).getBlock().equals(Blocks.LAVA)
-            ) {
-                pLevel.setBlock(bPos, Blocks.GLASS.defaultBlockState(), 3);
-            }
+            this.isActivated = !this.isActivated;
         }
         return InteractionResultHolder.sidedSuccess(pPlayer.getItemInHand(pHand), pLevel.isClientSide());
+    }
+
+    // Works, but too slow
+    /*@Override
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if(pEntity instanceof Player) {
+            if(isActivated) {
+                BlockPos bPos = pEntity.blockPosition().below();
+                if(!pLevel.isClientSide) {
+                    if(allowedBlocks.contains(pLevel.getBlockState(bPos).getBlock())) {
+                        pLevel.setBlockAndUpdate(bPos, Blocks.GLASS.defaultBlockState());
+                    }
+                }
+            }
+        }
+    }*/
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    @Override
+    public boolean isFoil(ItemStack pStack) {
+        return isActivated;
     }
 }
